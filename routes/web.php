07f5b272c99feb557c689b3e52b66ee8689f7a3e
Kerrial Newham <41723430+Kerrialn@users.noreply.github.com>
2018-10-02 12:15:24 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,27 @@
 |
 */
 
-
+// get landing page
 Route::get('/', 'AppController@index', function () {
 })->name('welcome');
 
+// return post search 
 Route::post('/search', 'AppController@front_search', function () {
 })->name('front.search');
 
+// get about page 
 Route::get('/about', 'AppController@about', function () {
 })->name('about');
 
-
+// Make sure Auth user is verified
 Auth::routes(['verify' => true]);
 
+// get Auth user home page 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 
 // rate resource
 Route::post('/rate/resource/{id}', 'HomeController@rate_resource', function ($id) {
 })->name('rate.resource');
-
 
 // new resource page
 Route::get('/resource/create', 'HomeController@getCreatePage', function () {
@@ -39,8 +41,6 @@ Route::get('/resource/create', 'HomeController@getCreatePage', function () {
 Route::post('/resource/create', 'HomeController@save_new_resource', function () {
 })->name('create.resource');
 
-
-
 // create new warm up
 Route::post('/resource/create-warmup/{id}', 'HomeController@create_warmup', function ($id) {
 })->name('new.warmup');
@@ -49,16 +49,12 @@ Route::post('/resource/create-warmup/{id}', 'HomeController@create_warmup', func
 Route::post('/resource/destroy-warmup/{id}', 'HomeController@destroy_warmup', function ($id) {
 })->name('warmup.destroy');
 
-
-
 // create new phrase 
 Route::post('/resource/create-phrase/{id}', 'HomeController@create_phrase', function ($id) {
 })->name('new.phrase');
 // destroy phrase 
 Route::post('/resource/destroy-phrase/{id}', 'HomeController@destroy_phrase', function ($id) {
 })->name('phrase.destroy');
-
-
 
 //create new meida
 Route::post('/resource/create-media/{id}', 'HomeController@create_media', function ($id) {
@@ -68,10 +64,6 @@ Route::post('/resource/create-media/{id}', 'HomeController@create_media', functi
 Route::post('/resource/destroy-media/{id}', 'HomeController@destroy_media', function ($id) {
 })->name('media.destroy');
 
-
-
-
-
 //create new article
 Route::post('/resource/create-article/{id}', 'HomeController@create_article', function ($id) {
 })->name('new.article');
@@ -80,16 +72,12 @@ Route::post('/resource/create-article/{id}', 'HomeController@create_article', fu
 Route::post('/resource/destroy-article/{id}', 'HomeController@destroy_article', function ($id) {
 })->name('article.destroy');
 
-
-
 //create new vocab
 Route::post('/resource/create-vocab/{id}', 'HomeController@create_vocab', function ($id) {
 })->name('new.vocab');
 // destroy vocab 
 Route::post('/resource/destroy-vocab/{id}', 'HomeController@destroy_vocab', function ($id) {
 })->name('vocab.destroy');
-
-
 
 //create new question
 Route::post('/resource/create-question/{id}', 'HomeController@create_question', function ($id) {
@@ -99,8 +87,6 @@ Route::post('/resource/create-question/{id}', 'HomeController@create_question', 
 Route::post('/resource/destroy-question/{id}', 'HomeController@destroy_question', function ($id) {
 })->name('question.destroy');
 
-
-
 //create new script
 Route::post('/resource/create-script/{id}', 'HomeController@create_script', function ($id) {
 })->name('new.script');
@@ -108,16 +94,12 @@ Route::post('/resource/create-script/{id}', 'HomeController@create_script', func
 Route::post('/resource/destroy-script/{id}', 'HomeController@destroy_script', function ($id) {
 })->name('script.destroy');
 
-
 //create new script
 Route::post('/resource/create-homework/{id}', 'HomeController@create_homework', function ($id) {
 })->name('new.homework');
 // destory script
 Route::post('/resource/destroy-homework/{id}', 'HomeController@destroy_homework', function ($id) {
 })->name('homework.destroy');
-
-
-
 
 //user account
 Route::get('/account', 'HomeController@getUserAccount', function () {
@@ -127,39 +109,29 @@ Route::get('/account', 'HomeController@getUserAccount', function () {
 Route::post('/user-update', 'HomeController@updateUserAccount', function () {
 })->name('update.user');
 
-
-
-
+// Auth user get resource page
 Route::get('/resource/view/{id}', 'HomeController@getResource', function ($id) {
 })->name('view.resource');
 
+// Auth user update resource
 Route::post('/resource/update/{id}', 'HomeController@update_resource', function ($id) {
 })->name('update.resource');
 
-
+// Auth user delete resource
 Route::post('/resource/delete/{id}', 'HomeController@destroy_resource', function ($id) {
 })->name('destroy.resource');
 
-
-
-
-
-
-
+// generate PDF for download
 Route::get('/download/{pdf}', 'DownloadController@downloadPDF', function ($filename) {
 })->name('download.pdf');
 
+// get resource by level
 Route::get('/resource/{level}', 'AppController@getLevel', function ($level) {
 })->name('resource.level');
 
-
+// get resource by level and id 
 Route::get('/resource/{level}/{id}', 'AppController@getItem', function ($level, $id) {
 })->name('resource.item');
 
-
-
-
-
-
-
+// logout Auth user 
 Route::get('logout', 'Auth\LoginController@logout');
